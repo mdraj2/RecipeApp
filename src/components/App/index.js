387c293e2283b63c4c0bbf1c14d6199f622bfcfc
Malith <malith.rajapakse@gmail.com>
2020-01-23 18:56:30 +1100

    refactor: App/index.js into clean code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,34 +6,64 @@ import axios from "./../../api/recipesapi";
 import "./base.scss";
 
 class App extends React.Component {
-  state = { results: [] };
-
-  componentDidUpdate() {}
-
-  //Get results when search button is clicked and update state of results array
-  getResults = async searchTerm => {
-    this.setState({ results: "loading" });
-    try {
-      const res = await axios.get(`api/search?&q=${searchTerm}`);
-      //Replace the unicode decimal code for apostrophe in the string recipes title
-      //e.g from "bill&#8217;s burger" to "bill's burger"
-      res.data.recipes.forEach((recipe, i) => {
-        res.data.recipes[i].title = recipe.title.replace("&#8217;", "'");
-      });
-      this.setState({ results: res.data.recipes });
-    } catch (err) {
-      this.setState({ results: "notfound" });
-    }
-  };
+  state = { recipes: [] };
 
   render() {
     return (
       <div className="main">
-        <Header getResults={this.getResults} />
-        <Results results={this.state.results} />
+        <Header getResults={this.getRawResults} />
+        <Results results={formatRecipeTitle(this.state.recipes)} />
       </div>
     );
   }
+
+  getRawResults = async searchTerm => {
+    try {
+      this.setState({ recipes: "loading" });
+      const recipesAPIresponseObject = await axios.get(
+        `api/search?&q=${searchTerm}`
+      );
+      const recipesDataArray = recipesAPIresponseObject.data.recipes;
+      this.setState({ recipes: recipesDataArray });
+    } catch (error) {
+      this.setState({ recipes: "notfound" });
+    }
+  };
 }
+
+/* Private functions */
+
+const formatRecipeTitle = recipes => {
+  if (Array.isArray(recipes)) {
+    const formattedRecipeArray = createNewFormatedRecipeTitleArray(recipes);
+    return formattedRecipeArray;
+  }
+  return recipes;
+};
+
+const createNewFormatedRecipeTitleArray = recipes => {
+  const newFormattedArray = recipes.map(singleRecipe => {
+    singleRecipe.title = convertUnicodeDecimalInRecipeTitle(singleRecipe.title);
+    return singleRecipe;
+  });
+  return newFormattedArray;
+};
+
+const convertUnicodeDecimalInRecipeTitle = title => {
+  const apostropheInUnicodeDecimalCode = "&#8217;";
+  return convertUnicodeDecimalCodeToCharacterString(
+    title,
+    apostropheInUnicodeDecimalCode
+  );
+};
+
+const convertUnicodeDecimalCodeToCharacterString = (
+  orignalString,
+  UnicodeDecimal
+) => {
+  let replaceUnicodeDecimalWith;
+  if (UnicodeDecimal === "&#8217;") replaceUnicodeDecimalWith = "'";
+  return orignalString.replace(UnicodeDecimal, replaceUnicodeDecimalWith);
+};
 
 export default App;
